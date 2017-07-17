93edbde34f573a03ee7ca81a7f6c4546fa7e3f80
haidy <haidy.duenas@gmail.com>
2017-07-17 11:57:17 -0700

    parte 1

---
+++ archivo.js
@@ -0,0 +1,15 @@
+
+if(planeta == "mercurio"){
+var peso = mercurio*masa;
+    mostrar("Hola" + nombre + ". Tu peso es  " + peso + " Klg en " + planeta);
+}
+
+if(planeta == "venus"){
+var peso = venus*masa;
+    mostrar("Hola" + nombre + ". Tu peso es  " + peso + " Klg en " + planeta);
+}
+
+if(planeta == "tierra"){
+var peso = tierra*masa;
+    mostrar("Hola" + nombre + ". Tu peso es  " + peso + " Klg en " + planeta);
+}
