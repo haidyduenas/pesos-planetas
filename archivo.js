810678ef14f2f63dd11eb1f8e0658689ff41445f
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-07-17 21:30:58 +0200

    agregado

--- archivo.js
+++ archivo.js
@@ -19,18 +19,14 @@ var peso = urano*masa;
      mostra("Hola" +nombre + ". Tu peso es " + peso + " kilogramos en " + planeta);
 }
 
-if(planeta == "nepturno"){
-var peso = nepturno*masa;
+if(planeta == "neptuno"){
+var peso = neptuno*masa;
      mostra("Hola" +nombre + ". Tu peso es " + peso + " kilogramos en " + planeta);
 }
-
-
-
 if(planeta == "mercurio"){
 var peso = mercurio*masa;
     mostrar("Hola" + nombre + ". Tu peso es  " + peso + " Klg en " + planeta);
 }
-
 if(planeta == "venus"){
 var peso = venus*masa;
     mostrar("Hola" + nombre + ". Tu peso es  " + peso + " Klg en " + planeta);
