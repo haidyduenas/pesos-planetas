0edc072ea56248b90587ae9c84edf4d2efa03364
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-07-17 20:58:21 +0200

    parte final

---
+++ archivo.js
@@ -0,0 +1,25 @@
+if(planeta == "marte"){
+var peso = marte*masa;
+    mostra("Hola" + nombre + ". Tu peso es " + peso + " kilogramos en " + planeta);
+}
+
+if(planeta == "jupiter"){
+var peso = jupiter*masa;
+     mostra("Hola" + nombre + ". Tu peso es " + peso + " kilogramos en " + planeta);
+}
+
+if(planeta == "saturno"){
+var peso = saturno*masa;
+    mostra("Hola" + nombre + ". Tu peso es " + peso + " kilogramos en " + planeta);
+}
+
+if(planeta == "urano"){
+var peso = urano*masa;
+     mostra("Hola" +nombre + ". Tu peso es " + peso + " kilogramos en " + planeta);
+}
+
+if(planeta == "nepturno"){
+var peso = nepturno*masa;
+     mostra("Hola" +nombre + ". Tu peso es " + peso + " kilogramos en " + planeta);
+}
+
