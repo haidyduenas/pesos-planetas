1d6184f0c58a99475eee65f76ae0c41250368017
paola <bernodiana@gmail.com>
2017-07-17 13:45:24 -0500

    parte1

---
+++ archivo.js
@@ -0,0 +1,21 @@
+var mostrar=function(texto){
+    document.write(texto);
+    document.write("<hr>");    
+}
+
+var mercurio=3;
+var venus=8;
+var tierra=10;
+var marte=4;
+var jupiter=25;
+var saturno=10;
+var urano=8;
+var neptuno=11;
+
+var nombre = prompt("Cuál es tu nombre?");
+
+var masa = parseInt(prompt("Cuál es tu masa?"));
+
+var planeta = prompt("¿Qué planeta te gustaría saber tu peso?");
+
+
